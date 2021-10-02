f831438c81b07e9db03feee785a41b1640b9b179
KarinaStabile <karina.stabile@gmail.com>
2021-10-02 17:42:29 -0400

    commit tree 68dd005119f1c25bd7bf9ecdae0e3c335e9636c3

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    // str.charAt(0).toUpperCase()
+    // return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+    
+  }
+  static titleize(str) {
+    let stayLowercase = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    str = str.split(' ');
+    return str.map(idx => {
+      if (str[0] === idx) {
+        return idx.charAt(0).toUpperCase() + idx.slice(1); 
+      }
+
+      else if (stayLowercase.includes(idx)) {
+        return idx;
+      }
+      else {
+        return idx.charAt(0).toUpperCase() + idx.slice(1); 
+      }
+    }).join(" "); 
+  }
 }
